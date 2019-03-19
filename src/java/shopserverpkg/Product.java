2368d9a1e3b59d2ac60d77827b4b14534d290f3b
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-03-19 19:25:33 +0000

    Product Class

---
+++ src/java/shopserverpkg/Product.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shopserverpkg;
+
+/**
+ *
+ * @author Nathan
+ */
+public class Product {
+    private float id;
+    private String name;
+    private float price;
+    private int quantity;
+    private String type;
+
+    public Product(float id, String name, float price, int quantity, String type) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.quantity = quantity;
+    }
+
+    /**
+     * @return the name
+     */
+    public float getID() {
+        return id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the product name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    /**
+     * @return the product type / category
+     */
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
